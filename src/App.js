4f80c5f90aa91f8b294bfbbfb8d82fcce4616295
Sudarshanan G <gsudarshanan96@gmail.com>
2021-05-03 11:49:56 +0530

    Updated API Call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import Weather from './Components/weather';
+import Weather from './Components/currentForcast';
+import DailyForcast from './Components/dailyForcast';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
 require('dotenv').config()
@@ -10,33 +11,42 @@ function App() {
   const[lat, setLat] = useState([]);
   const[long, setLong] = useState([]);
   const[data, setData] = useState([]);
+  const[dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
+    var tempObj = {};
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      await fetch(`${process.env.REACT_APP_API_URL}/onecall?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
       .then(res => res.json())
       .then(result => {
-        setData(result)
-        // console.log(result);
+        tempObj = result
       });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        tempObj.name = result.name;
+        console.log(tempObj);
+      });
+      setData(tempObj)
     }
     fetchData();
   }, [lat,long]);
 
   return (
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData= {data} />
-      ):(
-        <Dimmer active>
-            <Loader>Loading..</Loader>
-        </Dimmer>
-      )}
+      <div className="curr-forcast">
+        {(typeof data.name != 'undefined') ? (
+          <Weather weatherData= {data} />
+        ):(
+          <div></div>
+        )}
+      </div>
     </div>
   );
 }
