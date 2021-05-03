7f7f3dff36f07773258d74787a0576f6bf8428ad
Sudarshanan G <55589019+SudarshananG@users.noreply.github.com>
2021-05-03 11:54:25 +0530

    Update weather.js

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -8,21 +8,6 @@ const refresh = () =>{
 }
 
 const ExampleCard = ({ weatherData })=>(
-    // <Card className="card-body-content">
-    //     <Card.Content>
-    //         <div className="top">
-    //             <h3 className="header">{weatherData.name}</h3>
-    //             <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
-    //         </div>
-    //         <p>Temperature: { weatherData.main.temp } &deg;C</p>
-    //         <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-    //         <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-    //         <p>Description: {weatherData.weather[0].description}</p>
-    //         <p>Humidity: {weatherData.main.humidity} %</p>
-    //         <p>Day: {moment().format('dddd')}</p>
-    //         <p>Date: {moment().format('LLL')}</p>
-    //     </Card.Content>
-    // </Card>
 
     <div className="main">
         <div className="main-container">
